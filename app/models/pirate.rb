b8936d2536392767e6f5896418b7c25cede8b7f6
Mac Rowe <m.rowe42@gmail.com>
2021-04-06 19:30:21 -0400

    commit tree c97c8203a5093f8a7737e4817127c4d7e1d424ba

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,24 @@
 class Pirate
+
+    attr_accessor :name, :weight, :height
+
+    @@all = []
+
+    def initialze(params)
+        @name = params[:name]
+        @weight = params[:weight]
+        @height = params[:height]
+        @@all << self
+    end 
+
+    def self.all 
+        @@all
+    end
+
+    def self.clear
+        @@all.clear
+    end
+
+
+
 end
